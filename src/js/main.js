0ccb885013bbffa564c776d5f0acda811ab8672a
Gosia Siłkowska <silkowska.m@gmail.com>
2020-03-05 19:20:14 +0100

    css styles and js counting function

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,25 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// place your code below
+const addGlass= document.querySelector(".counter__button--add--js");
+const removeGlass= document.querySelector(".counter__button--remove--js");
+let glassCounter= document.querySelector(".counter__number--js");
+let count=0;
+const water= document.querySelector(".water");
 
+addGlass.addEventListener("click", (e) => {
+  count += 1;
+  glassCounter.innerHTML = count;
+});
 
+removeGlass.addEventListener("click", (e) => {
+  if(count>=1){
+  count -= 1;
+  glassCounter.innerHTML = count;
+  };
+});
+
+console.log(glassCounter.innerHTML)
 console.log(`Hello world!`);
 
 
