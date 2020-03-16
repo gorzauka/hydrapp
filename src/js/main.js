8109c1435b29501053860fe8ba447a95ab86d628
Gosia Siłkowska <silkowska.m@gmail.com>
2020-03-16 19:27:55 +0100

    feat: fixed js

--- src/js/main.js
+++ src/js/main.js
@@ -3,50 +3,55 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
-const addGlass= document.querySelector(".counter__button--add--js");
-const removeGlass= document.querySelector(".counter__button--remove--js");
-let glassCounter= document.querySelector(".counter__number--js");
-let count=0;
-const water= document.querySelector(".water");
-const date= new Date().toISOString().slice(0,10);
-const history = document.querySelector(".history__value");
+const addGlass = document.querySelector(".counter__button--add--js");
+const removeGlass = document.querySelector(".counter__button--remove--js");
+const glassCounter = document.querySelector(".counter__number--js");
+let count = 0;
+const water = document.querySelector(".water");
+const date = new Date().toISOString().slice(0, 10);
+const history = document.querySelector(".history__value--js");
+const historyEntry = document.createElement("li");
 
-if(date){
+if (localStorage.length > 0) {
+  addGlass.addEventListener("click", (e) => {
+    count += 1;
+    localStorage.setItem(date, JSON.stringify(count));
+    glassCounter.innerHTML = count;
+  });
 
-addGlass.addEventListener("click", (e) => {
-  count += 1;
-  glassCounter.innerHTML = count;
-  localStorage.setItem(date, count);
-});
-
-removeGlass.addEventListener("click", (e) => {
-  if(count>=1){
-  count -= 1;
-  glassCounter.innerHTML = count;
-  };
-  localStorage.setItem(date, count);
-});
-  
+  removeGlass.addEventListener("click", (e) => {
+    if (count >= 1) {
+      count -= 1;
+      localStorage.setItem(date, JSON.stringify(count));
+      glassCounter.innerHTML = count;
+    };
+  });
 }
-else{
-  count=0;
+else {
+  count = 0;
 }
 
-history.innerHTML= `W dniu ${date} wypiłeś ${localStorage.getItem(date)} szklanek wody`;
 
 
+for (let i = 0; i < localStorage.length; i++) {
+  let historyEntryData = `${localStorage.key(i)}: ${localStorage.getItem(localStorage.key(i))} szklanek.`;
+  const historyEntry = document.createElement("li");
+  historyEntry.innerHTML = historyEntryData;
+  history.appendChild(historyEntry);
+  historyEntry.style.cssText = "color:#fff; padding-top:14px; font-size:14px;)";
 
+}
 
 
 
